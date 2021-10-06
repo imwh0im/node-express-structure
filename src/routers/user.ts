2dd996008c0ba76b79139eccb352ece1c1963683
henry <48340699+imwh0im@users.noreply.github.com>
2021-10-06 23:01:18 +0900

    Add get userId and users

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -4,10 +4,24 @@ import UserService from '../services/user-service';
 
 const userRouter = Router();
 
+const userService = new UserService();
+
+userRouter.get('/', (req, res, next) => {
+  const result = userService.getUsers();
+  res.send(result);
+  next();
+});
+
 userRouter.post('/', (req, res, next) => {
-  const result = new UserService().createUser(req.body);
+  const result = userService.createUser(req.body);
   res.json(result);
   next();
 });
+
+userRouter.get('/:userId', (req, res, next) => {
+  const result = userService.getUser(<number> req.params.userId);
+  res.json(result);
+  next();
+})
 
 export default userRouter;
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,8 +1,20 @@
 import UserModel from "../models/user-model";
 
 export default class UserService {
+  private userModel = new UserModel();
+
+  public getUsers() {
+    const users = this.userModel.getUsers(); 
+    return users;
+  }
+
   public createUser(userData: Record<string, unknown>): Record<string, unknown>{
-    const user = new UserModel().insertUser(userData);
+    const user = this.userModel.insertUser(userData);
     return user
+  }
+
+  public getUser(userId: number) {
+    const user = this.userModel.getUser(userId);
+    return user;
   }
 }
