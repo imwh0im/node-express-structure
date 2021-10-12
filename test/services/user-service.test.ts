4d2901268b6edfbc738163f70045b07b8b08694b
henry <48340699+imwh0im@users.noreply.github.com>
2021-10-12 23:45:14 +0900

    Add todo test case

--- test/services/user-service.test.ts
+++ test/services/user-service.test.ts
@@ -1,2 +1,8 @@
 import UserService from "../../src/services/user-service";
 
+describe('user service test', () => {
+  it.todo('get users')
+  it.todo('get user')
+  it.todo('create user')
+  it.todo('update user')
+})
